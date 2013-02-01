a694880fe408baedca48de2a6989d2ee05722221
Julius.yu <julius.yu@thenetcircle.com>
2013-02-02 00:24:09 +0800

    use main to start server

---
+++ diy/raiseup/src/main.clj
@@ -0,0 +1,11 @@
+(ns main
+  (:gen-class)
+  (:use org.httpkit.server
+        raiseup.handler))
+
+(
+ defn -main [& args]
+(run-server (-> app)
+            {:port (Integer/parseInt (nth args 1 "8080")) :ip (first args)} )
+  (println (str "Server started. listen at " (nth args 1) " "  (first args)))
+)
