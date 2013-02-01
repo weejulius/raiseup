e532b00f8d89632fbbfa99d0449825fb9fa7aaa0
jyu <wee.jud@gmail.com>
2013-02-01 18:08:56 +0800

    add compojure support

---
+++ diy/raiseup/project.clj
@@ -0,0 +1,9 @@
+(defproject raiseup "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]]
+  :plugins [[lein-ring "0.7.1"]]
+  :ring {:handler raiseup.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.2"]]}})
---
+++ diy/raiseup/src/raiseup/handler.clj
@@ -0,0 +1,11 @@
+(ns raiseup.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
