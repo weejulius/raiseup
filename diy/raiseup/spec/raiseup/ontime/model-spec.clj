c4d6d966a03ea764deb962a8ac58c97b4b9346d1
Julius.yu <julius.yu@thenetcircle.com>
2013-02-04 23:31:33 +0800

    format and test model

--- diy/raiseup/spec/raiseup/ontime/model-spec.clj
+++ diy/raiseup/spec/raiseup/ontime/model-spec.clj
@@ -2,8 +2,15 @@
   (:use  speclj.core
          raiseup.ontime.model))
 
-(describe "task"
+(describe "An task"
           (it "has identifier"
-              (should= 1 (:task-id (->Task 1 "task 1" "ower" nil)))))
+              (should= 1 (:task-id (->Task 1 "task 1" "ower" nil))))
+          (it "has description"
+              (should= "task 2" (:description (->Task 2 "task 2" "owner" nil))))
+          (it "has owner(creator)"
+              (should= "me" (:task-owner (->Task 3 "task 3" "me" nil))))
+          (it "has created time"
+              (let [now (java.util.Date.)]
+               (should= now (:created-time (->Task 4 "task 4" "me" now))))))
 
 (run-specs)
--- diy/raiseup/src/raiseup/ontime/model.clj
+++ diy/raiseup/src/raiseup/ontime/model.clj
@@ -1,15 +1,23 @@
 (ns raiseup.ontime.model)
 
 
-(defprotocol Task-protocol "task is a goal which can be done with a short time, normally it is from 10 min to 40 min. it is possible that the actual i
-s more than the estimation, therefore we might try more than times to achieve the task"
-             (attempt [this attempt-id estimation current-time] "attempt to complete the task within the estimation, each task can be attempt times")
-             (stop-attempt [this attempt-id current-time stop-reason] "when the estimation is over or unexpected urgent things to handle, we stop the attempt"))
+(defprotocol Task-protocol "task is a goal which can be done with a short time,
+  normally it is from 10 min to 40 min. it is possible that the actual is more
+  than the estimation, therefore we might try more than one times to achieve the
+  task"
+             (attempt [this attempt-id estimation current-time]
+               "attempt to complete the task within the estimation,
+                each task can be attempt times")
+             (stop-attempt [this attempt-id current-time stop-reason]
+               "when the estimation is over or unexpected urgent things to
+                handle, we stop the attempt"))
 
 (defrecord Task
   [task-id description task-owner created-time]
   Task-protocol
   (attempt [this attempt-id estimation current-time]
-    {:attempt-id attempt-id :task-id task-id :status :attempte-started :start-time current-time :estimation estimation})
+    {:attempt-id attempt-id :task-id task-id :status :attempte-started
+     :start-time current-time :estimation estimation})
   (stop-attempt [this attempt-id current-time stop-reason]
-    {:attempt-id attempt-id :task-id task-id :status :attempte-done :stopped-time current-time} ))
+    {:attempt-id attempt-id :task-id task-id :status :attempt-done
+     :stopped-time current-time}))
