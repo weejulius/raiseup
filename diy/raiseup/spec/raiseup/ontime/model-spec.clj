dcacc52224aff1132d4524bd3b99c1358e38527a
Julius.yu <julius.yu@thenetcircle.com>
2013-02-05 23:01:42 +0800

    add validation of model

--- diy/raiseup/spec/raiseup/ontime/model-spec.clj
+++ diy/raiseup/spec/raiseup/ontime/model-spec.clj
@@ -3,14 +3,35 @@
          raiseup.ontime.model))
 
 (describe "An task"
-          (it "has identifier"
-              (should= 1 (:task-id (->Task 1 "task 1" "ower" nil))))
-          (it "has description"
-              (should= "task 2" (:description (->Task 2 "task 2" "owner" nil))))
-          (it "has owner(creator)"
-              (should= "me" (:task-owner (->Task 3 "task 3" "me" nil))))
-          (it "has created time"
-              (let [now (java.util.Date.)]
-               (should= now (:created-time (->Task 4 "task 4" "me" now))))))
+  (it "has identifier"
+    (should= 1 (:task-id (->Task 1 "task 1" "ower" nil))))
+  (it "has description"
+    (should= "task 2" (:description (->Task 2 "task 2" "owner" nil))))
+  (it "has owner(creator)"
+    (should= "me" (:task-owner (->Task 3 "task 3" "me" nil))))
+  (it "has created time"
+    (let [now (java.util.Date.)]
+      (should= now (:created-time (->Task 4 "task 4" "me" now))))))
+
+(describe "An task"
+  (it "must has an number identifier"
+    (should-throw (create-task nil "task 5" "owner1" nil))
+    (should-throw (create-task "a" "task 6" "owner2" nil))
+    (should-throw (create-task (Object.) "task 7" "owner3" nil)
+                  )))
+
+(describe "An task"
+  (it "must have description to state the goal"
+    (should-throw (create-task 1 nil "owner" nil))
+    (should-throw (create-task 1 1 "owner" nil))))
+                            
 
 (run-specs)
+
+
+
+
+
+
+
+
--- diy/raiseup/src/raiseup/ontime/model.clj
+++ diy/raiseup/src/raiseup/ontime/model.clj
@@ -5,12 +5,11 @@
   normally it is from 10 min to 40 min. it is possible that the actual is more
   than the estimation, therefore we might try more than one times to achieve the
   task"
-             (attempt [this attempt-id estimation current-time]
-               "attempt to complete the task within the estimation,
-                each task can be attempt times")
-             (stop-attempt [this attempt-id current-time stop-reason]
-               "when the estimation is over or unexpected urgent things to
-                handle, we stop the attempt"))
+  (attempt [this attempt-id estimation current-time]
+    "attempt to complete the task within the estimation,each task can be attempt times")
+  (stop-attempt [this attempt-id current-time stop-reason]
+    "when the estimation is over or unexpected urgent things to
+    handle, we stop the attempt"))
 
 (defrecord Task
   [task-id description task-owner created-time]
@@ -21,3 +20,9 @@
   (stop-attempt [this attempt-id current-time stop-reason]
     {:attempt-id attempt-id :task-id task-id :status :attempt-done
      :stopped-time current-time}))
+
+(defn create-task "create one task"
+  [task-id description task-owner created-time]
+  {:pre [(number? task-id)
+         (string? description)]}
+  (->Task task-id description task-owner created-time))
