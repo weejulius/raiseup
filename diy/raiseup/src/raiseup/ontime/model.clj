79881e976d379fec1fe9505be6211cf04d5abd68
jyu <wee.jud@gmail.com>
2013-02-18 17:36:33 +0800

    format comments

--- diy/raiseup/src/raiseup/ontime/model.clj
+++ diy/raiseup/src/raiseup/ontime/model.clj
@@ -1,10 +1,13 @@
 (ns raiseup.ontime.model)
 
-(defprotocol Task-protocol "task is a unit of work which can be done round 30 min,
+(defprotocol Task-protocol
+  "task is a unit of work which can be done round 30 min,
   and it has clear goal and is easy to determine if it is done, we can try again if
   the task is not achieved within estimation."
+
   (attempt [this attempt-id estimation current-time]
     "attempt to complete the task within the estimation")
+
   (stop-attempt [this attempt-id current-time stop-reason]
     "when the estimation is over or unexpected urgent things to
     handle, we stop the attempt"))
