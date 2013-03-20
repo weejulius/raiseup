79e395f35aa9633b4e843861e4bce04f03f35e12
jyu <wee.jud@gmail.com>
2013-03-20 09:50:48 +0800

    add write/get to/from leveldb performance test

--- diy/raiseup/test/raiseup/ontime/t_commandhandler.clj
+++ diy/raiseup/test/raiseup/ontime/t_commandhandler.clj
@@ -2,7 +2,7 @@
   (:use midje.sweet
         raiseup.ontime.commandhandler))
 
-(def leveldb (open-leveldb "/tmp/leveldb-test10" {}))
+(def leveldb (open-leveldb "/tmp/leveldb-test0" {}))
 
                                         ;(fact (write-to-leveldb leveldb "hello" "word") => nil)
 (fact "open level db"
@@ -19,10 +19,18 @@
                   :name "test"}]} (open-leveldb-for-ar level-db-root-dir {})) =>
                   nil?)
 
-(fact "performance"
+(fact "write performance"
       (println
-       (time
-        (dotimes [n 1000000]
-          (.put leveldb (to-bytes (str n) "UTF-8") (to-bytes "word" "UTF-8"))
-          )))
+           (time
+            (dotimes [n 100000]
+              (.put leveldb (to-bytes (str n) "UTF-8") (to-bytes "word" "UTF-8")))))
       => nil?)
+
+(fact "get performance"
+      (println
+           (time
+            (dotimes [n 100000]
+              (.get leveldb (to-bytes (str n) "UTF-8")))))
+      => nil?)
+
+(.close leveldb)
