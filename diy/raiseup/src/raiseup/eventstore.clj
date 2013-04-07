d9000110f236527380b916156afcfdeca406f55d
jyu <wee.jud@gmail.com>
2013-04-07 13:02:29 +0800

    roll back the removed code by mistake for store events add comments

--- diy/raiseup/src/raiseup/eventstore.clj
+++ diy/raiseup/src/raiseup/eventstore.clj
@@ -3,15 +3,15 @@
             [raiseup.storage :as store]
             [raiseup.base :as base]))
 
-(defn make-store-key-of-event-ids
+(defn- make-store-key-of-event-ids
   "used to make the key for the event ids in the storage,
-   event-ids is used to locate the events for ar quickly"
+   each ar ownes a couple of events, therefore event-ids is
+   used to locate the events in the event store for ar quickly"
   [ar-name-str ar-id-str]
   (str ar-name-str ar-id-str))
 
 (defn store-events-id-mapped-by-ar-id
-  "map the events for ar by the ar's id
-   in order to find the events for ar quickly"
+  "store the event id of ar'events so as to find the events for ar quickly"
   [ar-name-str ar-id-str event-ids db]
   (let [store-key (make-store-key-of-event-ids ar-name-str ar-id-str)
         event-ids-byte (store/find-value-by-key store-key db)
@@ -35,7 +35,7 @@
 
 
 (defn write-events-to-leveldb
-  "the events are stored in the leveldb and the event id is the key"
+  "the events are stored in the leveldb and the event id is the store key"
   [events events-db]
   (let [batch  (.createWriteBatch events-db)]
     (doseq [event events]
@@ -44,39 +44,42 @@
             (serialize event)))
     (.write events-db batch)))
 
-defn store-events
-^{:added "1.0"
-  :abbre "ar->aggregate root"
-  :doc "store the umcommitted events of aggregate root"}
-[ar-name ar-id new-events ar-eventids-db events-db]
-let [ar-name-str (name ar-name)
-     ar-id-str (str ar-id)
-     new-event-ids (map #(:event-id %) new-events)]
-(future (write-events-to-leveldb) new-events events-db)
+(defn store-events
+  ^{:added "1.0"
+    :abbre "ar->aggregate root"
+    :doc "store the umcommitted events of aggregate root"}
+  [ar-name ar-id new-events ar-eventids-db events-db]
+  (let [ar-name-str (name ar-name)
+        ar-id-str (str ar-id)
+        new-event-ids (map #(:event-id %) new-events)]
+    (future (write-events-to-leveldb) new-events events-db)
+    (store-events-id-mapped-by-ar-id ar-name-str ar-id-str new-event-ids
+                                     ar-eventids-db)))
+
 
 (defn read-event-ids
   "the events ids are required when fetching an aggregate root
    then reading the events for ar according the read event ids"
   [ar-name-str ar-id-str ar-event-ids-db]
   (if-let [event-ids-byte
-        (store/find-value-by-key
-         (make-store-key-of-event-ids ar-name-str ar-id-str)
-         ar-event-ids-db)]
-      (base/byte-to-int-array event-ids-byte)))
+           (store/find-value-by-key
+            (make-store-key-of-event-ids ar-name-str ar-id-str)
+            ar-event-ids-db)]
+    (base/byte-to-int-array event-ids-byte)))
 
 (defn read-event
-  "read a single event by event id"
+  "read a single event by event id from event store"
   [event-id-byte events-db]
   (if-let [event-byte (.get events-db event-id-byte)]
-     (deserialize event-byte)))
+    (deserialize event-byte)))
 
 
 (defn read-events
-  "read events for an ar, firstly read its event ids,
-   and then read events "
+  "used to read events for an ar, firstly read its event ids from storage,
+   and then read the actual events by the read event ids"
   [ar-name ar-id ar-event-ids-db events-db]
   (let [event-ids (read-event-ids (name ar-name) (str ar-id) ar-event-ids-db)]
     (map
      (fn [event-id]
-      (read-event (base/int-to-bytes [event-id]) events-db))
+       (read-event (base/int-to-bytes [event-id]) events-db))
      event-ids)))
