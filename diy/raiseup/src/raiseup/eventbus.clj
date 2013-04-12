318982732d96a7b3a7541a53d7cad40edeb68c69
jyu <wee.jud@gmail.com>
2013-04-12 13:00:46 +0800

    asyn to process event

--- diy/raiseup/src/raiseup/eventbus.clj
+++ diy/raiseup/src/raiseup/eventbus.clj
@@ -2,10 +2,12 @@
 
 
 (defn ->sent
-  ^{:doc "send events to their listeners"}
+  ^{:doc "send events to their listeners, if the execution
+          of event does not take much time, do not use pararellasm"}
   [event event-router]
   (doseq [queued-listeners ((:event event) event-router)]
-    (dorun (pmap #(% event) queued-listeners))))
+    (if-let [pros (last queued-listeners)]
+      (condp = pros))))
 
 (defn ->sub
   ^{:doc "subscribe event to listeners"}
--- diy/raiseup/test/raiseup/t_eventbus.clj
+++ diy/raiseup/test/raiseup/t_eventbus.clj
@@ -12,27 +12,51 @@
   [event]
   (reset! v 2))
 
+(defn on-task-created-heavy
+  [event]
+  (Thread/sleep 1000)
+  (reset! v 3))
+
+
+
 (def event-map
   {:task-created [[on-task-created1]
                   [on-task-created]]})
 
-
 (def event-map1
   {:task-created [[on-task-created on-task-created1]]})
+
+(def event-map2
+  {:task-created [[on-task-created1]
+                  [on-task-created-heavy {:asyn true}]]})
 
 (fact "publish event to handlers"
       (->sent {:event :task-created :event-id 1001} event-map)
       @v => 1)
 
 
-(fact "the listeners process event concurrently"
+(fact "the listeners process events"
       (count
        (filter #(= % 2)
-               (take 1000
+               (take 100000
                      (do
-                       (repeatedly #(do
-                                      (->sent {:event :task-created :event-id 1001} event-map1)
-                                      @v))))))=> (roughly 500 200))
+                       (repeatedly
+                        #(do
+                           (->sent {:event :task-created :event-id 1001} event-map1)
+                           @v))))))=> 100000)
+
+
+(fact "pararel to execute the task if the execution is heavy"
+      (time
+       (count
+        (filter #(= % 3)
+                (take 10
+                      (do
+                        (repeatedly
+                         #(do
+                            (->sent {:event :task-created :event-id 1001} event-map2)
+                            @v))))))) => 10)
+
 
 (fact "subscribe event"
       (->sub :task-created on-task-created))
