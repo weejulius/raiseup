29065bc2f60cba418de2ef2e71358958bdf1bc88
jyu <wee.jud@gmail.com>
2013-05-03 13:51:06 +0800

    [OK] refactor the main fun

--- diy/raiseup/src/main.clj
+++ diy/raiseup/src/main.clj
@@ -4,9 +4,16 @@
   (:require [ring.middleware.reload :as reload]
             [compojure.handler :refer [site]]))
 
-(defn -main [& args]
+(defn- start-server
+  [port-str ip]
   (let [handler (reload/wrap-reload (site #'app-routes))
-        port (Integer/parseInt (nth args 1 "8080"))
-        ip (nth args 0 "localhost")]
+        port (Integer/parseInt port-str)]
     (run-server handler {:port port :ip ip})
     (println (str "Server started. listen at " ip ":" port))))
+
+(defn -main [& args]
+  (start-server (nth args 1 "8080") (nth args 0 "localhost")))
+
+(defn start-server-in-dev-mode
+  []
+  (start-server "8080" "localhost"))
