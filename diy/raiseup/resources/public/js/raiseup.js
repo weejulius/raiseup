ebb316aec75c83b22ed361fa7e0ce6b58eda43de
jyu <wee.jud@gmail.com>
2013-05-09 13:01:25 +0800

    [OK]update to do list [OK]append new slots to the list after adding a new slot

--- diy/raiseup/resources/public/js/raiseup.js
+++ diy/raiseup/resources/public/js/raiseup.js
@@ -2,7 +2,7 @@ function readInputsToJson(){
   var json = {};
   for (var i = 0; i < arguments.length; i++) {
     inputName = arguments[i];
-    json[inputName]=$('input[name='+inputName+']').val()
+    json[inputName]=$('input[name='+inputName+']').val();
   }
   return json;
 }
@@ -20,15 +20,26 @@ function showAlert(el,msg){
   el.addClass("alert in error-msg");
 }
 
+function val(input){
+  var value = input.val();
+  if(value){
+    return value;
+  }else{
+    return '';
+  }
+}
+
 (function ($) {
   var websocket = $.websocket("ws://127.0.0.1:8080/todo/slots",{
     events:{
       message:function(e){
         if(e.data.errors){
-          showAlert($('#slot-new-msg'),e.data.errors)
+          showAlert($('#slot-new-msg'),e.data.errors);
         }else{
-          resetForm('module-new-slot')
-          $('#slot-new-msg').html('<i class="icon-ok"></i>')
+          var appendedSlot = '<li>'+val($('#description'))+' '+val($('#start-time'))+' '+val($('#estimation'))+'</li>';
+          $('#module-planned-slot-list').append(appendedSlot);
+          resetForm('module-new-slot');
+          $('#slot-new-msg').html('<i class="icon-ok"></i>');
           $('#slot-new-msg').addClass('in');
           window.setTimeout(function(){
             $('#slot-new-msg').removeClass('in');
--- diy/raiseup/todolist.md
+++ diy/raiseup/todolist.md
@@ -1,5 +1,9 @@
 
 
+* time slot associate with user
+* list slots when openning the new slot page
+* css the slots list
+* divide the slots into planned and unplanned
 
 * replay the events
 * move commandbus eventbus to a standalone module and abstract them
