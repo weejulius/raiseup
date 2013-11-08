0b4d6a50de22fdec08293e190fec88f703b3faef
jyu <wee.jud@gmail.com>
2013-11-08 19:58:42 +0800

    make the project work

--- diy/raiseup/src/common/func.clj
+++ diy/raiseup/src/common/func.clj
@@ -14,12 +14,11 @@
 (defn put-if-absence
   [m path new-entry]
   (update-in m path
-             (fn [entry]
-               )))
+             #((cond-update new-entry nil?) %)))
 
 (defn put-if-absence!
   "load entry from atom data structure, put one if it is not existing,
    the `path` is the path of the nested keys to get the entry"
   [entries path new-entry]
   (swap! entries
-         #))
+         (fn [m] (put-if-absence m path new-entry))))
--- diy/raiseup/test/common/t_func.clj
+++ diy/raiseup/test/common/t_func.clj
@@ -6,6 +6,7 @@
   (put-if-absence {} [:a] (fn [] (do (println "running ") 1))) => {:a 1})
 
 (fact "put if absence"
-  (put-if-absence! (atom {}) [:a] (fn [] (do (println "running ") 1))) => {:a 1}
+  (put-if-absence! (atom {:a 2}) [:a]
+                   (fn [] (do (println "running ") 1))) => {:a 2}
   (put-if-absence! (atom {}) [:a] 1) => {:a 1}
   (put-if-absence! (atom {}) [:a :b] 2) => {:a {:b 2}})
